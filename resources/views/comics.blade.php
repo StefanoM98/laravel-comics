be92dd910c603c18aacdc70c1f9ad408e15017a7
StefanoM98 <smartino030@gmail.com>
2023-05-23 15:22:40 +0200

    Add: foreach cycle in comics section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="comics-container py-4">
+        <div class="container">
+            <div class="text">
+                <h3>CURRENT SERIES</h3>
+            </div>
+            <div class="row row-col-6 gap-3">
+                @foreach ($db as $comics)
+                    <div class="col">
+                        <div class="card">
+                            <img src="{{ $comics['thumb'] }}" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $comics['title'] }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/comics', function () {
-    return view('comics');
+    $db = config('db');
+    return view('comics', compact('db'));
 });
 
 Route::get('/characters', function () {
