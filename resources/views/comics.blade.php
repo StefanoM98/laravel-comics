d3554f50deb13d1252aacb8bbef612944646f54c
StefanoM98 <smartino030@gmail.com>
2023-05-23 15:28:32 +0200

    Add: new db

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,13 +6,13 @@
             <div class="text">
                 <h3>CURRENT SERIES</h3>
             </div>
-            <div class="row row-col-6 gap-3">
+            <div class="row row-cols-6 p-3">
                 @foreach ($db as $comics)
                     <div class="col">
                         <div class="card">
                             <img src="{{ $comics['thumb'] }}" alt="">
                             <div class="card-body">
-                                <h5 class="card-title">{{ $comics['title'] }}</h5>
+                                <h5 class="card-title">{{ $comics['series'] }}</h5>
                             </div>
                         </div>
                     </div>
